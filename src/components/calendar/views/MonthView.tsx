4cb92d9665f6c765bcc7216e722c8fd9905b45f6
KHAIHWAN <Khai_Chiao@outlook.co.th>
2025-10-15 15:21:34 +0700

    ➕ specify the current date

--- src/components/calendar/views/MonthView.tsx
+++ src/components/calendar/views/MonthView.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client"
 
 // Import Components
-import { getDaysInMonth } from "@/utils/calendar"
+import { getDaysInMonth, isToday } from "@/utils/calendar"
 
 export default function MonthView({ currentDate }: { currentDate: Date }) {
 	const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
@@ -20,17 +20,24 @@ export default function MonthView({ currentDate }: { currentDate: Date }) {
 					</div>
 				))}
 			</div>
-            
-            {/* Calendar Grid */}
-            <div className="grid grid-cols-7 flex-1 border-l border-gray-200">
-                {days.map((day: Date, index: number) => (
-                    <div
-                        key={index}
-                        className='min-h-[120px] p-2 border-b border-r border-gray-200 last:border-r-0'>
-                        {day.getDate()}
-                    </div>
-                ))}
-            </div>
+
+			{/* Calendar Grid */}
+			<div className='grid grid-cols-7 flex-1 border-l border-gray-200'>
+				{days.map((day: Date, index: number) => (
+					<div
+						key={index}
+						className='min-h-[120px] p-2 border-b border-r border-gray-200 last:border-r-0'>
+						<div
+							className={
+								isToday(day)
+									? "w-7 h-7 flex items-center justify-center bg-black text-white p-2 rounded-full"
+									: ""
+							}>
+							{day.getDate()}
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	)
 }
--- src/utils/calendar.ts
+++ src/utils/calendar.ts
@@ -32,3 +32,15 @@ export const getDaysInMonth = (year: number, month: number): Date[] => {
     
     return days
 }
+
+export const isSameDay = (date1: Date, date2: Date): boolean => {
+    return (
+        date1.getDate() === date2.getDate() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getFullYear() === date2.getFullYear()
+    )
+}
+
+export const isToday = (date: Date): boolean => {
+    return isSameDay(date, new Date())
+}
